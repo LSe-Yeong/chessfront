af418ec034011b321be0c60449d5f4cc30fe7a71
Lse-Yeong <ocdee39@gmail.com>
2025-11-11 16:49:36 +0900

    feat: 기물을 눌렀을 때 기물의 색을 초록색으로 변경 기능 구현

--- src/game/page/ChessBoard.js
+++ src/game/page/ChessBoard.js
@@ -1,11 +1,16 @@
 import './ChessBoard.css'
 import {chessPiecesBlack,chessPiecesWhite} from '../model/ChessPiece.js'
+import { useState } from 'react';
 const chessSize = 8
 
 function ChessBlock(props) {
 
     const row = props.row
     const col = props.col
+    const blackPieces = props.blackPieces
+    const whitePieces = props.whitePieces
+    const setBlackPieces = props.setBlackPieces
+    const setWhitePieces = props.setWhitePieces
 
     function getColorByCoordinate() {
         if ((row + col) % 2 == 0) {
@@ -15,18 +20,30 @@ function ChessBlock(props) {
     }
 
     function findPiece() {
-        for (let i = 0; i < chessPiecesBlack.length; i++) {
-            if (chessPiecesBlack[i]["row"] == row && chessPiecesBlack[i]["col"] == col) {
+        const pieceBlackStyle = {
+            color: "black"
+        };
+        for (let i = 0; i < blackPieces.length; i++) {
+            if (blackPieces[i]["row"] == row && blackPieces[i]["col"] == col) {
+                if (blackPieces[i]["selected"]) {
+                    pieceBlackStyle["color"] = "green"
+                } 
                 return (
-                    <span style={{color:"black"}}>{chessPiecesBlack[i]["piece"]} </span>
+                    <span style={pieceBlackStyle}>{blackPieces[i]["piece"]} </span>
                 )
             }
         }
 
-        for (let i = 0; i < chessPiecesWhite.length; i++) {
-            if (chessPiecesWhite[i]["row"] == row && chessPiecesWhite[i]["col"] == col) {
+        const pieceWhiteStyle = {
+            color: "white"
+        };
+        for (let i = 0; i < whitePieces.length; i++) {
+            if (whitePieces[i]["row"] == row && whitePieces[i]["col"] == col) {
+                if (whitePieces[i]["selected"]) {
+                    pieceWhiteStyle["color"] = "green"
+                }
                 return (
-                    <span style={{color:"white"}}>{chessPiecesBlack[i]["piece"]} </span>
+                    <span style={pieceWhiteStyle}>{whitePieces[i]["piece"]} </span>
                 )
             }
         }
@@ -38,15 +55,43 @@ function ChessBlock(props) {
 
     return (
         <div className="chess-block" style={chessBlockStyle}>
-            <div className='piece'> {findPiece()} </div>
+            <div className='piece' onClick={() => {
+                const newBlackPieces = [...blackPieces];
+                for(let i = 0; i < newBlackPieces.length; i++) {
+                    if (newBlackPieces[i]["row"]==row && newBlackPieces[i]["col"] == col) {
+                        newBlackPieces[i]["selected"] = true
+                    } else {
+                        newBlackPieces[i]["selected"] = false
+                    }
+                }
+                setBlackPieces(newBlackPieces);
+                
+                const newWhitePieces = [...whitePieces];
+                for(let i = 0; i < newWhitePieces.length; i++) {
+                    if (newWhitePieces[i]["row"]==row && newBlackPieces[i]["col"] == col) {
+                        newWhitePieces[i]["selected"] = true
+                    } else {
+                        newWhitePieces[i]["selected"] = false
+                    }
+                }
+                setWhitePieces(whitePieces);
+            }}> {findPiece()} </div>
         </div>
     )
 }
 
 function ChessBoardRow(props) {
     const chessBoardRow = []
+
     for (let c = 0; c < chessSize; c++) {
-        chessBoardRow.push(<ChessBlock row={props.row} col={c}></ChessBlock>)
+        chessBoardRow.push(<ChessBlock 
+            row={props.row} 
+            col={c}
+            blackPieces = {props.blackPieces}
+            setBlackPieces = {props.setBlackPieces}
+            whitePieces = {props.whitePieces}
+            setWhitePieces = {props.setWhitePieces}
+        ></ChessBlock>)
     }
 
     return(
@@ -56,10 +101,19 @@ function ChessBoardRow(props) {
     )
 }
 
-function ChessBoard() {    
+function ChessBoard() {  
+    const [blackPieces, setBlackPieces] = useState(chessPiecesBlack)
+    const [whitePieces, setWhitePieces] = useState(chessPiecesWhite)
+ 
     const chessBoard = []
     for (let r = 0; r < chessSize; r++) {
-       chessBoard.push(<ChessBoardRow row={r}></ChessBoardRow>)
+       chessBoard.push(<ChessBoardRow 
+            row={r} 
+            blackPieces = {blackPieces} 
+            setBlackPieces = {setBlackPieces}
+            whitePieces = {whitePieces}
+            setWhitePieces = {setWhitePieces}
+        ></ChessBoardRow>)
     }
 
     return (
